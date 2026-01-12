0318cd3cc878dcc28f7bd0abde3e29c9aa485a3f
Supriya <supriyabhukya45@gmail.com>
2026-01-11 19:41:35 -0600

    i have changes the message with meaning full sentence

--- eCommerce.Core/Validators/RegisterRequestValidators.cs
+++ eCommerce.Core/Validators/RegisterRequestValidators.cs
@@ -11,7 +11,7 @@ namespace eCommerce.Core.Validators;
         RuleFor(temp => temp.Email).NotEmpty().WithMessage("Email Is A required")
             .EmailAddress().WithMessage("Invalid Email Formate");
         RuleFor(temp => temp.Password).NotEmpty().WithMessage("Password is Required");
-        RuleFor(temp => temp.PersonName).NotEmpty().WithMessage("PersonName  is Required");
+        RuleFor(temp => temp.PersonName).NotEmpty().WithMessage("PersonName  is Required and should not be empty");
         RuleFor(temp => temp.Gender).IsInEnum().WithMessage("Gender is Invalid");
     }
     }
